7a110688bfe55e436450e5a1357e0341b86a11a7
Bjorni Tare <btare99@gmail.com>
2026-04-28 15:16:21 +0200

    ndryshime

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,11 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express()
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
